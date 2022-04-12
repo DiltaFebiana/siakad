0b195058495b1b89de26c508acdfc600ffe0ef8d
DiltaFebiana <febianadilta@gmail.com>
2022-04-12 07:18:37 +0700

    Praktikum 9: ORM dengan relasi

--- resources/views/mahasiswa/edit.blade.php
+++ resources/views/mahasiswa/edit.blade.php
@@ -31,26 +31,16 @@
                 <label for="Nama">Nama</label> 
                 <input type="text" name="Nama" class="form-control" id="Nama" value="{{ $Mahasiswa->nama }}" aria-describedby="Nama" > 
             </div>
-            <div class="form-group">
-                <label for="Kelas">Kelas</label> 
-                <input type="Kelas" name="Kelas" class="form-control" id="Kelas" value="{{ $Mahasiswa->kelas }}" aria-describedby="Kelas" > 
-            </div>
+            <label for="Kelas">Kelas</label>
+            <select name="Kelas" class="form-control">
+                @foreach($kelas as $kls)
+                    <option value="{{$kls->id}}" {{ $Mahasiswa->kelas_id == $kls->id ? 'selected' : ''}}>{{$kls->nama_kelas}}</option>
+                @endforeach
+            </select>
             <div class="form-group">
                 <label for="Jurusan">Jurusan</label> 
                 <input type="Jurusan" name="Jurusan" class="form-control" id="Jurusan" value="{{ $Mahasiswa->jurusan }}" aria-describedby="Jurusan" > 
             </div>
-            <div class="form-group">
-                    <label for="Email">Email</label> 
-                    <input type="Email" name="Email" class="form-control" id="Email" value="{{ $Mahasiswa->Email }}" aria-describedby="Email" > 
-                </div>
-                <div class="form-group">
-                    <label for="Alamat">Alamat</label> 
-                    <input type="Alamat" name="Alamat" class="form-control" id="Alamat" value="{{ $Mahasiswa->Alamat }}" aria-describedby="Alamat" > 
-                </div>
-                <div class="form-group">
-                    <label for="Tanggal_lahir">Tanggal Lahir</label> 
-                    <input type="Tanggal_lahir" name="Tanggal_lahir" class="form-control" id="Tanggal_lahir" value="{{ $Mahasiswa->Tanggal_lahir }}" aria-describedby="Tanggal_lahir" > 
-                </div>
                 <button type="submit" class="btn btn-primary">Submit</button>
                 </form>
             </div>
